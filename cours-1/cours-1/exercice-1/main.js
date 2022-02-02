88c2b86f42e4a0cfb5c49986033715c58334861f
simoncbouchard <simoncbouchard@gmail.com>
2022-02-02 09:09:00 -0500

    Delete cours-1/cours-1 directory

--- cours-1/cours-1/exercice-1/main.js
+++
@@ -1,8 +0,0 @@
-/**
- * Au chargement de la page, injecter la valeur de l'attribut 'maxlength' de l'input text dans l'élement <span data-js-input-limit>
- * À la saisi d'un caractère dans l'input text, afficher dynamiquement le nombre restant de caractère(s) avant d'atteindre la limite récupérée précédemment
- * Assurez-vous que la chaîne 'caractères' soit au pluriel seulement si le nombre restant est supérieur à 1
- * Au click du bouton 'Soumettre', si une valeur est saisie, injecter celle-ci dans l’élément <input>
- * ainsi que la longueur de cette même valeur
- */
-
